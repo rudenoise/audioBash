13f606c651aaea314e0e632d2b67b20365b7211a
rudenoise <rudenoise@gmail.com>
2017-03-31 10:46:29 +0100

    adjust for clipping

--- 24to16.bash
+++ 24to16.bash
@@ -11,13 +11,10 @@ cp -r $DIR_FROM* $DIR_TO
 
 # convert files to 16 bit
 find $DIR_TO -type f -name "*.wav" | \
-    xargs -I file sox file -r 44100 -b 16 file.16.wav
+    xargs -I file sox file -r 44100 -b 16 -G file.16.wav
 
 # remove original
 find audio/to/ -type f -not -name "*16.wav" | xargs rm
 
 # rename converted
 find -name "*.16.wav" -type f | rename 's/.16.wav//g'
-
-
-# sox CL_Trex1.wav -r 44100 -b 8 CL_Trex1-8.wav
